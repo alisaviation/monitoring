15d9f1e723bf8941014aede8763c310a51ec6f01
eaobolenskaya <eaobolenskaya@avito.ru>
2025-04-21 22:09:13 +0300

    iter1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/alisaviation/monitoring
+
+go 1.22.7
---
+++ internal/storage/memstorage.go
@@ -0,0 +1,32 @@
+package storage
+
+type MemStorage struct {
+	//mu             sync.RWMutex
+	gauges   map[string]float64
+	counters map[string]int64
+}
+
+func NewMemStorage() *MemStorage {
+	return &MemStorage{
+		gauges:   make(map[string]float64),
+		counters: make(map[string]int64),
+	}
+}
+
+func (m *MemStorage) SetGauge(name string, value float64) {
+	m.gauges[name] = value
+}
+
+func (m *MemStorage) AddCounter(name string, value int64) {
+	m.counters[name] += value
+}
+
+func (m *MemStorage) GetGauge(name string) (float64, bool) {
+	value, exists := m.gauges[name]
+	return value, exists
+}
+
+func (m *MemStorage) GetCounter(name string) (int64, bool) {
+	value, exists := m.counters[name]
+	return value, exists
+}
