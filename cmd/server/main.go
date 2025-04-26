4ca0d2dd910099bfbaf476b80d014fbd2498d54a
eaobolenskaya <eaobolenskaya@avito.ru>
2025-04-26 22:20:48 +0300

    iter5

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,7 +4,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
-	"time"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 
@@ -13,9 +13,9 @@ import (
 )
 
 func main() {
-	addr := flag.String("a", "localhost:8080", "HTTP server endpoint address")
-	interval := flag.Duration("i", 10*time.Second, "Interval for some periodic task (in seconds)")
-	timeout := flag.Duration("t", 5*time.Second, "Timeout for some operation (in seconds)")
+	defaultAddress := "localhost:8080"
+	addressEnv := os.Getenv("ADDRESS")
+	address := flag.String("a", defaultAddress, "HTTP server endpoint address")
 
 	flag.Parse()
 
@@ -23,19 +23,24 @@ func main() {
 		log.Fatalf("Unknown flags: %v", flag.Args())
 	}
 
+	var serverAddress string
+	if addressEnv != "" {
+		serverAddress = addressEnv
+	} else {
+		serverAddress = *address
+	}
+
 	memStorage := storage.NewMemStorage()
-	if err := run(memStorage, *addr, *interval, *timeout); err != nil {
+	if err := run(memStorage, serverAddress); err != nil {
 		log.Fatalf("Error running server: %v", err)
 	}
 }
 
-func run(memStorage *storage.MemStorage, addr string, interval, timeout time.Duration) error {
+func run(memStorage *storage.MemStorage, addr string) error {
 	r := chi.NewRouter()
 	r.Post("/update/{type}/{name}/{value}", helpers.MethodCheck([]string{http.MethodPost})(updateMetrics(memStorage)).(http.HandlerFunc))
 	r.Get("/value/{type}/{name}", helpers.MethodCheck([]string{http.MethodGet})(getValue(memStorage)).(http.HandlerFunc))
 	r.Get("/", getMetricsList(memStorage))
-
-	log.Printf("Server will use interval: %v and timeout: %v", interval, timeout)
 
 	return http.ListenAndServe(addr, r)
 }
