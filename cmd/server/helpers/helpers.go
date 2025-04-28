f91edd8f165af8ebc631a93e4fd4ad3c57ac4c18
eaobolenskaya <eaobolenskaya@avito.ru>
2025-04-28 19:01:32 +0300

    iter3 success tests

--- cmd/server/helpers/helpers.go
+++ cmd/server/helpers/helpers.go
@@ -3,21 +3,23 @@ package helpers
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func WriteResponse(w http.ResponseWriter, statusCode int, body interface{}) {
-	w.Header().Set("Content-Type", "text/plain")
+	//w.Header().Set("Content-Type", "text/plain")
 	w.WriteHeader(statusCode)
 
 	switch v := body.(type) {
 	case float64:
-		w.Write([]byte(fmt.Sprintf("%.2f", v)))
+		fmt.Fprintf(w, "%s", FormatFloat(v))
 	case int64:
-		w.Write([]byte(fmt.Sprintf("%d", v)))
+		fmt.Fprintf(w, "%d", v)
 	default:
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
 }
+
 func MethodCheck(methods []string) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -30,4 +32,9 @@ func MethodCheck(methods []string) func(next http.Handler) http.Handler {
 			http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		})
 	}
+}
+
+func FormatFloat(value float64) string {
+	formatted := fmt.Sprintf("%.3f", value)
+	return strings.TrimRight(strings.TrimRight(formatted, "0"), ".")
 }
